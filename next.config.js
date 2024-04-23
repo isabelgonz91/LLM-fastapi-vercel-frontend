d53b31fdeaa7a2d73c1a35b0eb0982002a781d42
isabelgonz91 <36759536+isabelgonz91@users.noreply.github.com>
2024-04-23 16:19:04 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
+// Importa el módulo 'path' para resolver rutas
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+
+  // Extiende la configuración de Webpack
+  webpack: (config) => {
+    // Configura un alias '@' que apunta a la raíz del directorio del proyecto
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
