a4c18a61c210f18083acade08dcb3bb3897cbdff
isabelgonz91 <36759536+isabelgonz91@users.noreply.github.com>
2024-04-23 14:31:46 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ import ToDoList from '../components/todo-list';
 
 export default function Home() {
   return (
-    <div>
-     <Head>
-       <title>Full Stack Book To Do</title>
-       <meta name="description" content="Full Stack Book To Do" />
-       <link rel="icon" href="/favicon.ico" />
-     </Head>
-     <Layout>
-       <ToDoList />
-     </Layout>
-    </div>
+    <Layout>
+      <div>
+        <Head>
+          <title>Full Stack Book To Do</title>
+          <meta name="description" content="Full Stack Book To Do" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <ToDoList />
+      </div>
+    </Layout>
   )
 }
